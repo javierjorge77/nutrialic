2c92932d71d71af1bc0c11f242932c257a5af3a3
jorge perez <javierjorge77@gmail.com>
2022-10-27 21:56:50 -0500

    index de profesionales un poco mas bonito

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,17 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
 user1 = User.create(name:"Cristel", lastname: "Alvarez", phone: "7773388548", password: "12345678", email: "cristelalvarezvazquez@gmail.com")
+Professional.create(user: user1, speciallity: "Nutrición Clínica", adress: "Cuautla", diploma: "10206015", first_cost: 500, follow_cost: 350)
 
-Professional.create(user: user1, speciallity: "Nutrición Clínica", adress: "La pradera Cuernavaca", diploma: "10206015", first_cost: 500, follow_cost: 350)
+user2 = User.create(name:"Noemi ", lastname: "Landa", phone: "7774398683", password: "12345678", email: "nlg.0619@hotmail.com")
+Professional.create(user: user2, speciallity: "Nutrición Clínica", adress: "Avenida Cuauhtemoc #170-A local 9 Plaza Romanza", diploma: "10206006", first_cost: 400, follow_cost: 150)
 
-# create_table "professionals", force: :cascade do |t|
-#   t.string "speciallity"
-#   t.string "adress"
-#   t.string "diploma"
-#   t.integer "first_cost"
-#   t.integer "follow_cost"
-#   t.bigint "user_id", null: false
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-#   t.index ["user_id"], name: "index_professionals_on_user_id"
-# end
+user3 = User.create(name:"Nivia", lastname: "Esquivel", phone: "7772073576", password: "6555940", email: "diaxesquivel@gmail.com")
+Professional.create(user: user3, speciallity: "Nutrición Clínica, Educadora en Diabetes ", adress: "Avenida Sn Diego 1203. CP 62330,- Hospital San Diego Cuernavaca , Morelos. Consultorio 406", diploma: "6555940", first_cost: 600, follow_cost: 400)
 
-# create_table "users", force: :cascade do |t|
-#   t.string "email", default: "", null: false
-#   t.string "encrypted_password", default: "", null: false
-#   t.string "reset_password_token"
-#   t.datetime "reset_password_sent_at"
-#   t.datetime "remember_created_at"
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-#   t.boolean "nutritionist"
-#   t.string "name"
-#   t.string "lastname"
-#   t.string "phone"
-#   t.index ["email"], name: "index_users_on_email", unique: true
-#   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-# end
+user4 = User.create(name:"Ana", lastname: "Acuña", phone: "5517005252", password: "12345678", email: "charpinel7@gmail.com")
+Professional.create(user: user4, speciallity: "Nutrición Clínica", adress: "Puebla", diploma: "13065988", first_cost: 500, follow_cost: 400)
+
+user5 = User.create(name:"Jessica", lastname: "Penagos", phone: "9611141121", password: "12345678", email: "Penagos_jl@hotmail.com")
+Professional.create(user: user5, speciallity: "Nutrición Clínica, Obesidad, Educación en Diabetes", adress: "Puebla", diploma: "11977457", first_cost: 450, follow_cost: 350)
