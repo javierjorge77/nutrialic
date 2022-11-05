e609c2e89e65c80c8fcd1437448b01ae5cfe16d5
JuanPedro Toro <jpdungeon@gmail.com>
2022-11-05 13:16:44 -0300

    casi rdy

--- app/javascript/controllers/map_show_controller.js
+++ app/javascript/controllers/map_show_controller.js
@@ -12,15 +12,22 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10",
+      zoom: 10,
+      center: [ this.markerValue.lng, this.markerValue.lat ]
     })
     this.#addMarkersToMap()
+    this.#fitMapToMarker()
   }
 
   #addMarkersToMap() {
       new mapboxgl.Marker()
         .setLngLat([ this.markerValue.lng, this.markerValue.lat ])
         .addTo(this.map)
-
+  }
+  #fitMapToMarker() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markerValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
