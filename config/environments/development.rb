cb897bebb5ae495a3f28957f15eb0c48e7cf2110
Daniel <daniel_carrillo_2003@outlook.com>
2023-06-03 11:01:55 -0600

    Test load of images

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+ 
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
@@ -61,6 +63,9 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.assets.debug = true
+  config.assets.digest = false
+
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   #config.action_mailer.default_url_options = { host: 'https://nutrialic.herokuapp.com/', port: 3000 }
