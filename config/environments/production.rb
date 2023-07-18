7bf12ad47a12bbd61ab00d615b5b2d079081c9c6
Daniel <daniel_carrillo_2003@outlook.com>
2023-07-18 13:47:34 -0600

    Deliver later mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,11 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
 
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { host: "https://nutrialic.herokuapp.com/" }
+config.action_mailer.default_url_options = { host: "https://www.nutrialic.com/" }
+config.action_mailer.smtp_settings = {
+  enable_starttls_auto: true,  
+  ssl: true                    
+}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
